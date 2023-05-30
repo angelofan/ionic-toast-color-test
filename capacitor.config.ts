769da65b0824a018e60079bb3549ac4c7030172c
angelofan <1253264809@qq.com>
2023-05-30 10:55:22 +0800

    create

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.toast.color.test',
+  appName: 'ionic-toast-color-test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
